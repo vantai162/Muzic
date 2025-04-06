6204f156b75851bc4d56c46372a9e4e2eadfe05c
hoang tai <hoangvantai162@gmail.com>
2025-04-06 11:15:05 +0700

    Them class Song va them mau,icon

---
+++ app/src/main/java/com/example/muzic/ui/Song.java
@@ -0,0 +1,37 @@
+package com.example.muzic.ui;
+
+public class Song {
+    private int id;
+    private String title;
+
+    private String youtubeUrl;
+    public Song(int _id,String _title,String _URL){
+        id = _id;
+        title = _title;
+        youtubeUrl = _URL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYoutubeUrl() {
+        return youtubeUrl;
+    }
+
+    public void setYoutubeUrl(String youtubeUrl) {
+        this.youtubeUrl = youtubeUrl;
+    }
+}
